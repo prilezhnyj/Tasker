2e089e59c6bf6af61ca836c986ce4c58bb6bd5b6
Maksim Botalov <dev.botalov@gmail.com>
2022-09-28 02:07:15 +0500

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Максим Боталов on 28.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
