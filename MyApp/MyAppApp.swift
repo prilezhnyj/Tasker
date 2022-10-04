1773e28b7aacc407e6adaa416616cb92902cd211
Maksim Botalov <dev.botalov@gmail.com>
2022-10-04 14:59:21 +0500

    Firebase Integration.

--- MyApp/MyAppApp.swift
+++ MyApp/MyAppApp.swift
@@ -6,12 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct MyAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             StartView()
         }
     }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
